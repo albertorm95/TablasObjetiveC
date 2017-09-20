39e779d5347c47c87620be709b5b81ff1cbe8218
econfig <econfig@econfigs-macbook-air-5.local>
2017-09-20 18:24:57 -0500

    Inicial Comió

--- Tablas/Home.m
+++ Tablas/Home.m
@@ -6,13 +6,13 @@
 //  Copyright © 2017 amrojas. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "Home.h"
 
-@interface ViewController ()
+@interface Home ()
 
 @end
 
-@implementation ViewController
+@implementation Home
 
 - (void)viewDidLoad {
     [super viewDidLoad];
