e9b2a660c036c9ac1d38b9043f14cf2e5ac2dbf2
econfig <econfig@econfigs-macbook-air-5.local>
2017-09-20 18:22:17 -0500

    Initial Commit

---
+++ Tablas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tablas
+//
+//  Created by econfig on 9/20/17.
+//  Copyright © 2017 amrojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tablas/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tablas
+//
+//  Created by econfig on 9/20/17.
+//  Copyright © 2017 amrojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
